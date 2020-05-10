3b8e16ce75618c35fd6317de7cf94dd232fd088a
saad-pixel <marva.saad2@gmail.com>
2020-05-10 08:30:24 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,17 @@
+func sayRan( num : Int, num2 : Int	) -> Int {
+	
+	
+	let result = num * num2
+	
+
+return result
+
+}
+
+
+
+  for i in 1...3 {  
+    
+    print (sayRan(num :2, num2: 3))
+    
+  }
